4721c191abd7f4a875fa62f3c05f34b989e80bff
SoubirSaian <soubir2018@gmail.com>
2022-02-20 16:51:08 +0600

    copy-color-code

--- app.js
+++ app.js
@@ -1,4 +1,14 @@
- 
+
+function myFunction(){
+    let copyText = document.getElementById('colorCode');
+    
+    navigator.clipboard.writeText(copyText.innerText);
+    document.execCommand('copy');
+    window.alert("copied the text" + copyText.innerText);
+    
+    console.log(copyText.innerHTML);
+}
+
 var array = ["0","1","2","3","4","5","6","7","8","9","a","b","c","d","e","f"];
 var color = "#";
 
@@ -10,10 +20,18 @@ function colorGenerator(){
 
 for(var j=1; j<=5; j++){
     colorGenerator();
+    
     document.getElementById("div"+j).style.backgroundColor = color;
-    document.getElementById("div"+j).innerHTML = color;
+     
+    document.getElementById("div"+j).innerHTML = `<div>
+        <h2 id="colorCode">${color}</h2>
+        <button onclick="myFunction()">Copy</button>
+    </div>`;
+    
     color = "#";
 }
+
+
 
 document.addEventListener('keyup', function(e){
     if(e.keyCode == 32){
@@ -21,4 +39,3 @@ document.addEventListener('keyup', function(e){
     }
 });
 
-// document.getElementById("button").onclick(location.reload );
